82c43fdf13648463404f523f767a1f706417869f
ryr3 <ryr397@gmail.com>
2016-09-16 23:35:30 +0530

    Add lvl

--- Enemies.js
+++ Enemies.js
@@ -1,23 +1,20 @@
-var	enemies = [],
-	path = [],
-	timelog = [],
-	list=[43,7,5,44,45,45,45,19];
-for(var count = 0; count < list.length; count++) {
-	var pizza=list[count];	
-	enemies.push({
-		x : random(obstacles[pizza].x, obstacles[pizza].x + obstacles[pizza].width - 16),
-		y: obstacles[pizza].y - 20,
-		width: 16,
-		height: 20,
-		velX: Math.round(random(0, 1)) - 0.5,
-		velY: 0,
-		sight: false,
-		firing: false
-	});
-	timelog[count - 4] = null;
-	path[count - 4] = null;
-}
+const enemies = [
+	{
+		x: 800,
+		y: 610,
+		vel: {
+			x: 100,
+			y: 0
+		}
+	},
+	{
+		x: 1600,
+		y: 880,
+		vel: {
+			x: 100,
+			y: 0
+		}
+	}
+];
 
-function random(min, max) {
-	return min + (max - min)*Math.random();
-}
+export default enemies;
